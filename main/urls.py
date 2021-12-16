9997c951b0659c0573662a198eb1894d2e63fddd
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-12-16 18:11:43 +0300

    add urls

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,22 @@
-from django.contrib import admin
-from django.urls import path, include #add include
+# stdlib  imports
+
+# django imports
+from django.urls import path
+
+# 3rd party imports
+
+# project imports
+from djangoflutterwave.views import (
+    TransactionCreateView,
+    TransactionDetailView,
+    PaymentParamsView,
+)
+
+
+app_name = "djangoflutterwave"
 
 urlpatterns = [
-  path('', include('main.urls')),  #add path
-  path('admin/', admin.site.urls),
+    path("transaction/", TransactionCreateView.as_view(), name="transaction_create"),
+    path("payment-params/", PaymentParamsView.as_view(), name="payment_params"),
+    path("<str:tx_ref>/", TransactionDetailView.as_view(), name="transaction_detail"),
 ]
